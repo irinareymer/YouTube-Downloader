6f5efed4d96faa360c91bea459cf5a45c6b561f5
Reymer Irina <reymer.irina4@gmail.com>
2022-11-30 19:06:35 +0300

    #7 Added output data from backend

--- backend/downloader/views.py
+++ backend/downloader/views.py
@@ -8,9 +8,13 @@ from downloader import utils
 @api_view()
 def youtube_download(request):
     url = request.query_params.get('url', None)
+    file_type = request.query_params.get('type', None)
     data = utils.YoutubeData(url)
     title = data.video_title()
     duration = data.video_duration()
-    video_link = data.video_downloader()
-    audio_link = data.audio_downloader()
-    return Response(status=status.HTTP_200_OK)
+    link = None
+    if file_type == 'video':
+        link = data.video_downloader()
+    elif file_type == 'audio':
+        link = data.audio_downloader()
+    return Response({"title": title, "duration": duration, "link": link}, status=status.HTTP_200_OK)
